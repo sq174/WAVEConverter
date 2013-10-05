fd2de2c2944a6a7d23584b4508263264e57540d8
yk <git@dhdo.net>
2013-10-05 14:40:26 +0900

    update
    ウインドウの表示

---
+++ tkinter_tester.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import tkinter
+from tkinter import *
+
+tk = tkinter.Tk()
+
+f = LabelFrame(tk, text = 'ファイル選択',
+               width = 150, height = 60, labelanchor = NW)
+f.pack(padx = 10, pady = 10, side = LEFT)
