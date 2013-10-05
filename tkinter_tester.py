e5e941fb4e03570cdb3d64a152f22b522f7fdda0
yk <git@dhdo.net>
2013-10-05 17:43:41 +0900

    update
    ファイル選択ダイアログを追加

--- tkinter_tester.py
+++ tkinter_tester.py
@@ -4,7 +4,30 @@ import tkinter
 from tkinter import *
 
 tk = tkinter.Tk()
+tk.title('WAVEConverter')
+filenames = StringVar()
+filenames.set("")
 
-f = LabelFrame(tk, text = 'ファイル選択',
-               width = 150, height = 60, labelanchor = NW)
-f.pack(padx = 10, pady = 10, side = LEFT)
+def select_files():
+    filenames.set(filedialog.askopenfilenames(filetypes = [('CUE Files', ('.cue'))],
+                                              initialdir = 'D:/temp', 
+                                              title = "ファイル選択"))
+
+#ファイル選択のフレーム
+f = LabelFrame(tk, text = 'ファイル選択', labelanchor = NW)
+#ファイル名を表示するエントリー
+e = Entry(f, textvariable = filenames, borderwidth = 2)
+e.pack(padx = 5, pady = 5, side = LEFT, expand = True, fill = X)
+#ファイル選択ダイアログを開くボタン
+a = Button(f, text = '...', width = 5, font = (20), 
+           command = select_files)
+a.pack(padx = 5, pady = 5, side = LEFT)
+f.pack(padx = 10, pady = 10, expand = True, fill = X)
+#ボタンを含むフレーム
+
+tk.mainloop()
+
+
+
+
+    
