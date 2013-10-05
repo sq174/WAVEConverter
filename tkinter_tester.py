706c10813f38d073e2c78593cb21bb7a342063ac
yk <git@dhdo.net>
2013-10-05 22:33:41 +0900

    bugfix
    サブウィンドウ関係の調整

--- tkinter_tester.py
+++ tkinter_tester.py
@@ -2,17 +2,19 @@
 
 import tkinter
 from tkinter import *
-
+import sys, os.path
 tk = tkinter.Tk()
 tk.title('WAVEConverter')
+#tk.minsize(width = tk.winfo_width(), height = tk.winfo_height())
+
 filenames = StringVar()
 filenames.set("")
-
 def select_files():
     filenames.set(filedialog.askopenfilenames(filetypes = [('CUE Files', ('.cue'))],
                                               initialdir = 'D:/temp', 
                                               title = "ファイル選択"))
-
+def show_logwindow():
+    return
 #LabelFrame:ファイル選択
 f0 = LabelFrame(tk, text = 'ファイル選択', labelanchor = NW)
 #Entry:ファイル名を表示
@@ -28,15 +30,18 @@ f0.pack(padx = 10, pady = 10, expand = True, fill = X)
 f1 = Frame(tk)
 #Button:変換開始
 a = Button(f1, text = '変換', width = 5,
-           command = select_files)
+           command = show_logwindow)
 a.pack(padx = 10, side = LEFT, anchor = CENTER)
 #Button:閉じる
+"""
 a = Button(f1, text = '閉じる', width = 5,
-           command = sys.exit)
+           command = lambda : sys.exit())
 a.pack(padx = 10, side = LEFT, anchor = CENTER)
+"""
 f1.pack(padx = 10, pady = 10, anchor =  CENTER, expand = True, fill = Y)
 
 tk.mainloop()
+
 
 
 
