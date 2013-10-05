2bf02926facc9767eea8165f133ad6ca5e59f02b
yk <git@dhdo.net>
2013-10-05 18:12:02 +0900

    update
    ウィンドウのひな形が完成

--- tkinter_tester.py
+++ tkinter_tester.py
@@ -13,17 +13,28 @@ def select_files():
                                               initialdir = 'D:/temp', 
                                               title = "ファイル選択"))
 
-#ファイル選択のフレーム
-f = LabelFrame(tk, text = 'ファイル選択', labelanchor = NW)
-#ファイル名を表示するエントリー
-e = Entry(f, textvariable = filenames, borderwidth = 2)
+#LabelFrame:ファイル選択
+f0 = LabelFrame(tk, text = 'ファイル選択', labelanchor = NW)
+#Entry:ファイル名を表示
+e = Entry(f0, textvariable = filenames, borderwidth = 2)
 e.pack(padx = 5, pady = 5, side = LEFT, expand = True, fill = X)
-#ファイル選択ダイアログを開くボタン
-a = Button(f, text = '...', width = 5, font = (20), 
+#Button:ファイル選択ダイアログを開く
+a = Button(f0, text = '...', width = 5, 
            command = select_files)
 a.pack(padx = 5, pady = 5, side = LEFT)
-f.pack(padx = 10, pady = 10, expand = True, fill = X)
-#ボタンを含むフレーム
+f0.pack(padx = 10, pady = 10, expand = True, fill = X)
+
+#Frame:ボタン含
+f1 = Frame(tk)
+#Button:変換開始
+a = Button(f1, text = '変換', width = 5,
+           command = select_files)
+a.pack(padx = 10, side = LEFT, anchor = CENTER)
+#Button:閉じる
+a = Button(f1, text = '閉じる', width = 5,
+           command = sys.exit)
+a.pack(padx = 10, side = LEFT, anchor = CENTER)
+f1.pack(padx = 10, pady = 10, anchor =  CENTER, expand = True, fill = Y)
 
 tk.mainloop()
 
